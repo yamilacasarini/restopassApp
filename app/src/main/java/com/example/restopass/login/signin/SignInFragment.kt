3ce25f02d22ad59328e583eee88fbb902748bca9
Juan Cabanas <juani_cabanas@hotmail.com>
2020-05-10 19:16:30 -0300

    scroll view added

--- app/src/main/java/com/example/restopass/login/signin/SignInFragment.kt
+++ app/src/main/java/com/example/restopass/login/signin/SignInFragment.kt
@@ -5,15 +5,52 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.example.restopass.R
+import com.example.restopass.databinding.FragmentSigninBinding
+import kotlinx.android.synthetic.main.fragment_forgot_password.*
 import kotlinx.android.synthetic.main.fragment_signin.*
+import kotlinx.android.synthetic.main.fragment_signin.view.*
 
 class SignInFragment : Fragment() {
+
     private var listener: OnFragmentInteractionListener? = null
+
+    private lateinit var viewModel: SignInViewModel
+    private lateinit var binding: FragmentSigninBinding
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_signin, container, false)
+
+        // Inflate view and obtain an instance of the binding class
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_signin,
+            container,
+            false
+        )
+
+        viewModel = ViewModelProvider(requireActivity()).get(SignInViewModel::class.java)
+
+        binding.signInViewModel = viewModel
+
+        binding.restoPassSignInButton.setOnClickListener {
+            updateViewModel()
+        }
+
+
+        return binding.root
+    }
+
+    private fun updateViewModel() {
+        binding.apply {
+            viewModel.email = emailInput.text.toString()
+            viewModel.password = passwordInput.text.toString()
+            invalidateAll()
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -22,10 +59,6 @@ class SignInFragment : Fragment() {
 
         forgotPasswordButton.setOnClickListener {
             listener?.showFragment(ForgotPasswordFragment())
-        }
-
-        restoPassSignInButton.setOnClickListener {
-            listener?.signIn()
         }
     }
 
---
+++ app/src/main/java/com/example/restopass/login/signin/SignInViewModel.kt
@@ -0,0 +1,5 @@
+package com.example.restopass.login.signin
+
+import androidx.lifecycle.ViewModel
+
+class SignInViewModel(var email: String = "", var password: String = "") : ViewModel()
