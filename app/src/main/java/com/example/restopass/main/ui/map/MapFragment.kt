d0ad9d2b73064bfee24e1f6fa8bfeac21d2623e3
Tobias Voskoboinik <tobiasvosk@gmail.com>
2020-05-23 13:04:00 -0300

    add location

--- app/src/main/java/com/example/restopass/main/ui/map/MapFragment.kt
+++ app/src/main/java/com/example/restopass/main/ui/map/MapFragment.kt
@@ -1,23 +1,30 @@
 package com.example.restopass.main.ui.map
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
 import android.widget.TextView.OnEditorActionListener
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.findNavController
 import com.example.restopass.R
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
 import kotlinx.android.synthetic.main.fragment_map.*
 
 
@@ -25,15 +32,18 @@ class MapFragment : Fragment(), OnMapReadyCallback{
 
     private lateinit var mapViewModel: MapViewModel
     private var mMap: GoogleMap? = null
+    private val fineLocation = Manifest.permission.ACCESS_FINE_LOCATION
+    private val coarseLocation = Manifest.permission.ACCESS_COARSE_LOCATION
+    private val permissions = arrayOf(fineLocation, coarseLocation)
+    private val permissionCode = 1234
+    private var locationGranted = false
+    private var location: LatLng? = null
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         mapViewModel =
             ViewModelProviders.of(this).get(MapViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_map, container, false)
+        initializeLocation()
         return root
     }
 
@@ -44,7 +54,6 @@ class MapFragment : Fragment(), OnMapReadyCallback{
         map_search.setEndIconOnClickListener {
             view.findNavController().navigate(R.id.filterFragment)
         }
-
         map_search_edit.setOnEditorActionListener(OnEditorActionListener { v, actionId, event ->
             if (actionId == EditorInfo.IME_ACTION_SEARCH) {
                 Toast.makeText(this.context, "search action", Toast.LENGTH_SHORT).show()
@@ -57,11 +66,48 @@ class MapFragment : Fragment(), OnMapReadyCallback{
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
         // Add a marker in Sydney, Australia, and move the camera.
-        val sydney = LatLng((-34).toDouble(), 151.0)
-        mMap?.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap?.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        // val currentLocation: Location =
+        //   LocationServices.FusedLocationApi.getLastLocation(googleApiClient)
+//        val sydney = LatLng((-34).toDouble(), 151.0)
+//        mMap?.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+//        mMap?.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        mMap?.isMyLocationEnabled = true
     }
 
+    private fun initializeLocation() {
+        locationGranted = getLocationPermissions()
+        if (!locationGranted)
+            requestLocationPermission()
+        else
+            getLocation()
+    }
 
+    private fun getLocation() {
+        val fuseLoc = this.context?.let { LocationServices.getFusedLocationProviderClient(it) }
+        if (locationGranted) {
+            fuseLoc?.lastLocation?.addOnSuccessListener  {lastLocation : Location? ->
+                lastLocation?.let {
+                    val latLng = LatLng(it.latitude, it.longitude)
+                    location?:apply { moveCamera(latLng, 15f) }
+                    location = latLng
+                }
+            }
+        }
+    }
+
+    private fun moveCamera(loc: LatLng, zoom: Float) = mMap?.moveCamera(CameraUpdateFactory.newLatLngZoom(loc, zoom))
+
+    private fun getLocationPermissions() = permissions.all { perm -> this.context?.let { ContextCompat.checkSelfPermission(it, perm) } == PackageManager.PERMISSION_GRANTED }
+
+    private fun requestLocationPermission() = this.activity?.let { ActivityCompat.requestPermissions(it, permissions, permissionCode) }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when (requestCode) {
+            permissionCode -> if (grantResults.isNotEmpty() && grantResults.all { it ==  PackageManager.PERMISSION_GRANTED }) {
+                locationGranted = true
+                getLocation()
+            }
+        }
+    }
 
 }
