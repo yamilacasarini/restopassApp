a24b48c558542a2d3214ef8dd8e940f8ffb74f0a
Tobias <tobiasvosk@gmail.com>
2020-06-21 12:36:34 -0300

    rating start view

--- app/src/main/java/com/example/restopass/main/common/restaurant/DishAdapterRating.kt
+++ app/src/main/java/com/example/restopass/main/common/restaurant/DishAdapterRating.kt
@@ -1,19 +1,10 @@
 package com.example.restopass.main.common.restaurant
 
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
-import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
-import com.example.restopass.R
-import com.example.restopass.common.AppPreferences
-import com.example.restopass.common.orElse
 import com.example.restopass.domain.Dish
-import com.example.restopass.domain.Membership
 import kotlinx.android.synthetic.main.dish_item.view.*
 
-class DishAdapterRating(private val dishes: List<Dish>) : DishAdapter(dishes) {
+class DishAdapterRating(private val dishes: List<Dish>) : DishAdapter(dishes, false) {
 
     override fun onBindViewHolder(holder: DishViewHolder, position: Int) {
         super.onBindViewHolder(holder, position)
