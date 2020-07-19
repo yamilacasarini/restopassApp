b6edae9c288a0ae3e883ce556ac9ad5da86775ac
Yamila Casarini <yamilacasarini@gmail.com>
2020-07-18 23:48:08 -0300

    adding b2cUsers (#8)
    * adding b2cUsers
    
    * change to b2b
    
    * fix for pr

--- app/src/main/java/com/example/restopass/main/common/membership/MembershipAdapter.kt
+++ app/src/main/java/com/example/restopass/main/common/membership/MembershipAdapter.kt
@@ -1,18 +1,24 @@
 package com.example.restopass.main.common.membership
 
-import android.util.TypedValue
+import android.content.res.ColorStateList
+import android.graphics.Paint
+import android.os.Build
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
+import androidx.core.view.ViewCompat
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.airbnb.paris.extensions.style
 import com.bumptech.glide.Glide
 import com.example.restopass.R
+import com.example.restopass.common.AppPreferences
 import com.example.restopass.domain.Membership
 import com.example.restopass.main.ui.home.notEnrolledHome.NotEnrolledHomeFragment
+import com.example.restopass.utils.AlertDialogUtils
+import kotlinx.android.synthetic.main.activity_main.view.*
 import kotlinx.android.synthetic.main.view_membership_item.view.*
-import java.lang.ClassCastException
 
 class MembershipAdapter(private val parentFragment: MembershipAdapterListener) :
     RecyclerView.Adapter<RecyclerView.ViewHolder>() {
@@ -29,17 +35,18 @@ class MembershipAdapter(private val parentFragment: MembershipAdapterListener) :
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        return when(viewType) {
+        return when (viewType) {
             VIEWTYPE_TITLE -> TitleViewHolder.from(parent)
             VIEWTYPE_MEMBERSHIP -> MembershipViewHolder.from(parent, memberships)
             else -> throw ClassCastException("Unknown viewType $viewType")
         }
     }
 
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         val membership = memberships[position]
 
-        if (membership.isTitle)  {
+        if (membership.isTitle) {
             holder as TitleViewHolder
             holder.bind(membership)
         } else {
@@ -48,7 +55,9 @@ class MembershipAdapter(private val parentFragment: MembershipAdapterListener) :
         }
     }
 
-    class MembershipViewHolder(val view: View, val memberships: List<Membership>) : RecyclerView.ViewHolder(view) {
+    class MembershipViewHolder(val view: View, val memberships: List<Membership>) :
+        RecyclerView.ViewHolder(view) {
+        @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
         fun bind(membership: Membership, parentFragment: MembershipAdapterListener, position: Int) {
             view.apply {
 
@@ -56,6 +65,36 @@ class MembershipAdapter(private val parentFragment: MembershipAdapterListener) :
 
                 membershipTitle.text = membership.name
                 priceTag.text = resources.getString(R.string.price_tag, membership.price.toString())
+
+                AppPreferences.user.b2BUserEmployee?.percentageDiscountPerMembership?.get(membership.membershipId!!)
+                    ?.let {
+                        when {
+                            it == 100F -> {
+                                grayPrice()
+                                fillPriceDiscountInfo(
+                                    resources.getString(R.string.price_free),
+                                    AppPreferences.user.b2BUserEmployee!!.companyName,
+                                    it.toInt().toString()
+                                )
+                            }
+                            it != 0F -> {
+                                grayPrice()
+                                fillPriceDiscountInfo(
+                                    resources.getString(
+                                        R.string.price_tag,
+                                        (membership.price!!.toFloat() * (100F - it) / 100F).toInt()
+                                            .toString()
+                                    ),
+                                    AppPreferences.user.b2BUserEmployee!!.companyName,
+                                    it.toInt().toString()
+                                )
+                            }
+                            else -> {
+                                priceDiscount.visibility = View.GONE
+                                priceInfo.visibility = View.GONE
+                            }
+                        }
+                    }
 
                 description.text = membership.description
 
@@ -85,11 +124,41 @@ class MembershipAdapter(private val parentFragment: MembershipAdapterListener) :
                 }
 
                 if (parentFragment is NotEnrolledHomeFragment) {
-                    if (position == memberships.size -1)  membershipCard.style(R.style.membershipVerticalLastCard)
+                    if (position == memberships.size - 1) membershipCard.style(R.style.membershipVerticalLastCard)
                     else membershipCard.style(R.style.membershipVerticalCard)
                 }
             }
         }
+
+        private fun grayPrice() {
+            view.apply {
+                priceTag.style(R.style.priceTagDark)
+                priceTag.paintFlags = priceTag.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
+            }
+        }
+
+        private fun fillPriceDiscountInfo(
+            msgDiscount: String,
+            discount: String,
+            companyName: String
+        ) {
+            view.apply {
+                priceInfo.setOnClickListener {
+                    AlertDialogUtils.buildAlertDialog(
+                        null,
+                        LayoutInflater.from(context),
+                        container,
+                        msg = resources.getString(
+                            R.string.price_alert, discount, companyName
+                        )
+                    ).show()
+                }
+                priceDiscount.text = msgDiscount
+                priceDiscount.visibility = View.VISIBLE
+                priceInfo.visibility = View.VISIBLE
+            }
+        }
+
         companion object {
             fun from(parent: ViewGroup, memberships: List<Membership>): MembershipViewHolder {
                 val layoutInflater = LayoutInflater.from(parent.context)
