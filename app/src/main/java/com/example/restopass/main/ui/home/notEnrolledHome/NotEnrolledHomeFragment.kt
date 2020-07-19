11ea94447e8fb57c53f87ed1e5b0741919d2e86e
Juan Cabanas <juani_cabanas@hotmail.com>
2020-07-19 01:38:50 -0300

    not enrolled with cc

--- app/src/main/java/com/example/restopass/main/ui/home/notEnrolledHome/NotEnrolledHomeFragment.kt
+++ app/src/main/java/com/example/restopass/main/ui/home/notEnrolledHome/NotEnrolledHomeFragment.kt
@@ -15,7 +15,10 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.restopass.R
 import com.example.restopass.common.AppPreferences
 import com.example.restopass.common.EmojisHelper
+import com.example.restopass.common.orElse
+import com.example.restopass.connection.Api4xxException
 import com.example.restopass.domain.*
+import com.example.restopass.main.common.AlertCreditCard
 import com.example.restopass.main.common.AlertDialog
 import com.example.restopass.main.common.LocationService
 import com.example.restopass.main.common.membership.MembershipAdapter
@@ -25,6 +28,7 @@ import com.example.restopass.main.common.restaurant.DishAdapterListener
 import com.example.restopass.main.common.restaurant.restaurantsList.RestaurantAdapter
 import com.example.restopass.main.common.restaurant.restaurantsList.RestaurantAdapterListener
 import com.example.restopass.main.ui.home.HomeViewModel
+import com.example.restopass.main.ui.settings.payment.PaymentViewModel
 import com.example.restopass.utils.AlertDialogUtils
 import com.google.android.gms.maps.model.LatLng
 import kotlinx.android.synthetic.main.activity_main.*
@@ -40,7 +44,9 @@ class NotEnrolledHomeFragment : Fragment(), RestaurantAdapterListener, Membershi
 
     private lateinit var restaurantRecyclerView: RecyclerView
     private lateinit var restaurantAdapter: RestaurantAdapter
+
     private lateinit var selectedRestaurantViewModel: RestaurantViewModel
+    private lateinit var paymentViewModel: PaymentViewModel
 
     private lateinit var homeViewModel: HomeViewModel
     private lateinit var membershipsViewModel: MembershipsViewModel
@@ -64,11 +70,11 @@ class NotEnrolledHomeFragment : Fragment(), RestaurantAdapterListener, Membershi
         super.onViewCreated(view, savedInstanceState)
 
         homeViewModel = ViewModelProvider(requireActivity()).get(HomeViewModel::class.java)
-        membershipsViewModel =
-            ViewModelProvider(requireActivity()).get(MembershipsViewModel::class.java)
+        membershipsViewModel = ViewModelProvider(requireActivity()).get(MembershipsViewModel::class.java)
 
-        selectedRestaurantViewModel =
-            ViewModelProvider(requireActivity()).get(RestaurantViewModel::class.java)
+        selectedRestaurantViewModel = ViewModelProvider(requireActivity()).get(RestaurantViewModel::class.java)
+
+        paymentViewModel = ViewModelProvider(requireActivity()).get(PaymentViewModel::class.java)
 
         membershipAdapter = MembershipAdapter(this)
         membershipRecyclerView = homeMembershipRecycler.apply {
@@ -120,6 +126,10 @@ class NotEnrolledHomeFragment : Fragment(), RestaurantAdapterListener, Membershi
             if (LocationService.isLocationGranted()) {
                 deferred.add(getRestaurantsByLocation())
             }
+            if (paymentViewModel.creditCard == null) {
+                deferred.add(getUserCreditCard())
+            }
+
             deferred.awaitAll()
 
             val isSignUp = requireActivity().intent?.getBooleanExtra("signUp", false)
@@ -203,11 +213,40 @@ class NotEnrolledHomeFragment : Fragment(), RestaurantAdapterListener, Membershi
         }
     }
 
+    private fun getUserCreditCard(): Deferred<Unit> {
+        return coroutineScope.async {
+            try {
+                paymentViewModel.get()
+            } catch (e: Api4xxException) {
+                if (e.error?.code != 40405) throw e
+            } catch (e: Exception) {
+                if (isActive) {
+                    Timber.e(e)
+                    membershipLoader.visibility = View.GONE
+                    AlertDialogUtils.buildAlertDialog(e, layoutInflater, container, view).show()
+                }
+            }
+        }
+    }
+
     override fun onEnrollClick(membership: Membership) {
+        paymentViewModel.creditCard?.let {
+            AlertDialog.getActionDialogWithParams(
+                context,
+                layoutInflater, notEnrolledContainer, ::updateMembership,
+                membership, AlertCreditCard(resources, it, membership.name)
+            ).show()
+        }.orElse {
+            membershipsViewModel.selectedUpdateMembership = membership
+            findNavController().navigate(R.id.paymentFragment)
+        }
+    }
+
+    private fun updateMembership(membership: Any) {
         membershipLoader.visibility = View.VISIBLE
         coroutineScope.launch {
             try {
-                membershipsViewModel.update(membership)
+                membershipsViewModel.update(membership as Membership)
                 membershipsViewModel.wasEnrolled = true
                 listener?.onEnrollClick()
             } catch (e: Exception) {
