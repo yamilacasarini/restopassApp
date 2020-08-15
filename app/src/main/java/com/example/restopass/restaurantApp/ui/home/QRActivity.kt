6b4d7f8b0ac6eef7c372b680c443aa74549221dd
Yamila Casarini <yamila.casarini@despegar.com>
2020-08-14 21:28:57 -0300

    fix qr activity

--- app/src/main/java/com/example/restopass/restaurantApp/ui/home/QRActivity.kt
+++ app/src/main/java/com/example/restopass/restaurantApp/ui/home/QRActivity.kt
@@ -1,30 +1,8 @@
 package com.example.restopass.restaurantApp.ui.home
 
-import android.app.Activity
-import android.content.Intent
-import androidx.navigation.findNavController
-import com.example.restopass.R
-import com.example.restopass.main.MainActivity
-import com.google.zxing.integration.android.IntentIntegrator
 import com.journeyapps.barcodescanner.CaptureActivity
-import kotlinx.android.synthetic.main.activity_restaurant.*
 
 
 class QRActivity : CaptureActivity() {
-
-    val activity : Activity? = null;
-
-    override fun onActivityResult(
-        requestCode: Int,
-        resultCode: Int,
-        data: Intent?
-    ) {
-        val result = IntentIntegrator.parseActivityResult(
-            requestCode,
-            resultCode, data
-        )
-            findNavController(R.id.nav_host_fragment_restaurant).navigate(R.id.restaurantDishesFragment)
-
-    }
 
 }
