c13f9390d417779f0deea2e64ae546ca5bff226a
Yamila Casarini <yamila.casarini@despegar.com>
2020-10-14 18:32:14 -0300

    fix boom

--- app/src/main/java/com/example/restopass/main/ui/settings/myvisits/MyVisitsFragment.kt
+++ app/src/main/java/com/example/restopass/main/ui/settings/myvisits/MyVisitsFragment.kt
@@ -10,12 +10,19 @@ import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import com.example.restopass.R
 import com.example.restopass.common.AppPreferences
 import com.example.restopass.domain.MembershipsViewModel
 import com.example.restopass.main.MainActivity
+import com.example.restopass.utils.AlertDialogUtils
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_my_visits.*
 import kotlinx.android.synthetic.main.fragment_my_visits.view.*
+import kotlinx.android.synthetic.main.fragment_my_visits.view.myVisitsMembershipText
+import kotlinx.android.synthetic.main.fragment_reservations.*
+import kotlinx.coroutines.*
+import timber.log.Timber
 import java.time.LocalDateTime
 
 class MyVisitsFragment : Fragment() {
@@ -28,6 +35,8 @@ class MyVisitsFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_my_visits, container, false)
     }
 
+    val job = Job()
+    val coroutineScope = CoroutineScope(job + Dispatchers.Main)
     private lateinit var membershipsViewModel: MembershipsViewModel
 
     @RequiresApi(Build.VERSION_CODES.O)
@@ -37,6 +46,22 @@ class MyVisitsFragment : Fragment() {
         membershipsViewModel =
             ViewModelProvider(requireActivity()).get(MembershipsViewModel::class.java)
 
+        if(membershipsViewModel.actualMembership == null) {
+            coroutineScope.launch {
+                try {
+                    membershipsViewModel.get()
+                    myVisitsMembershipText.text = Html.fromHtml(context?.getString(R.string.myVisitsMembership, membershipsViewModel.actualMembership!!.name, membershipsViewModel.actualMembership!!.visits.toString()))
+                } catch (e: Exception) {
+                    if (isActive) {
+                        Timber.e(e)
+                        AlertDialogUtils.buildAlertDialog(e, layoutInflater, container).show()
+                    }
+                }
+            }
+        } else {
+            myVisitsMembershipText.text = Html.fromHtml(context?.getString(R.string.myVisitsMembership, membershipsViewModel.actualMembership!!.name, membershipsViewModel.actualMembership!!.visits.toString()))
+        }
+
         (activity as MainActivity).topAppBar?.apply {
             setTitle(R.string.myVisitsTitle)
             visibility = View.VISIBLE
@@ -45,12 +70,12 @@ class MyVisitsFragment : Fragment() {
         view.apply {
             visitsAmount.text = AppPreferences.user.visits.toString()
             myVisitsExpiration.text =  Html.fromHtml(context.getString(R.string.myVisitsExpiration, calculateExpirationDate(AppPreferences.user.membershipEnrolledDate!!)))
-            myVisitsMembershipText.text = Html.fromHtml(context.getString(R.string.myVisitsMembership, membershipsViewModel.actualMembership!!.name, membershipsViewModel.actualMembership!!.visits.toString()))
             myVisitsMembershipBtn.setOnClickListener{
                 findNavController().navigate(R.id.membershipsFragment)
             }
         }
     }
+
 
     @RequiresApi(Build.VERSION_CODES.O)
     private fun calculateExpirationDate(date: String): String? {
